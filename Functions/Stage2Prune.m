197060dc22af520dc1e9ba4c689c5bb589198dda
msjaarda <mjsjaarda@gmail.com>
2021-08-23 11:05:43 +0200

    x

---
+++ Functions/Stage2Prune.m
@@ -0,0 +1,11 @@
+function [PD] = Stage2Prune(PD)
+%STAGE2PRUNE
+
+PD(PD.GW_TOT<6000,:) = [];
+% Only do the disqualification if we actually have SW10 Classification
+if sum(PD.CS == 2 | PD.CS == 3 | PD.CS == 4 | PD.CS == 6) < 0.7*height(PD)
+    PD(PD.CS == 2 | PD.CS == 3 | PD.CS == 4 | PD.CS == 6,:) = [];
+end
+
+end
+
