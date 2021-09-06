1280723e6171a7429e74a434e910fc361ee0e1cb
msjaarda <mjsjaarda@gmail.com>
2021-09-06 14:34:21 +0200

    PushtoENACvm

--- ComparatorILLib.m
+++ ComparatorILLib.m
@@ -6,7 +6,7 @@ load("ILLib.mat");
 
 disp("-----------------------------------------------------");
 disp("Bonjour bienvenue dans le script COMPARATOR");
-disp("Vous utilisez la version 2.1 du 19/08/2021, Lucas");
+disp("Vous utilisez la version 2.3 du 02/09/2021, Lucas");
 disp("-----------------------------------------------------");
 a = 1;
 
@@ -15,20 +15,32 @@ a = 1;
 while a ~= 0
 
 % Type de pont
-disp('-----------------------------');
+b = 0;
+while b == 0
+disp("-----------------------------------------------------");
 Prompt = 'Type de pont : 1)Box 2)Twin 3)Multi 4)Slab \n';
 Entry = input(Prompt);
-fprintf(repmat('\b',1,length(Prompt)+31));
+fprintf(repmat('\b',1,length(Prompt)+55));
+if Entry > 0 && Entry < 5
+b = 1;
+end
+end
 Donnee.BridgeType = {'Box';'Twin';'Multi';'Slab'};
 Donnee.BridgeType = char(Donnee.BridgeType(Entry));
 Fields = fieldnames(ILLib);
 Fields = Fields(contains(Fields,Donnee.BridgeType));
 Fields = flip(Fields);
 
-disp('-----------------------------');
+b = 0;
+while b == 0
+disp("-----------------------------------------------------");
 Prompt = 'Type de données : 1)New 2)AGB \n';
 Entry = input(Prompt);
-fprintf(repmat('\b',1,length(Prompt)+31));
+fprintf(repmat('\b',1,length(Prompt)+55));
+if Entry > 0 && Entry < 3
+b = 1;
+end
+end
 Donnee.TypDonn = {'';'AGB'};
 Donnee.TypDonn = char(Donnee.TypDonn(Entry));
 Fields = Fields(Entry);
@@ -38,8 +50,10 @@ Path = append('ILLib.',char(Fields));
 
 % Sous type du pont
 if isempty(Donnee.TypDonn) || (strcmp(Donnee.BridgeType,'Twin') && strcmp(Donnee.TypDonn,'AGB'))
+b = 0;
+while b == 0
 disp(append('Disposition ',int2str(a),' : ',Path));
-disp('-----------------------------');
+disp("-----------------------------------------------------");
 Prompt = 'Sous type du pont : ';
 Fields = fieldnames(eval(Path));
     for i = 1:height(Fields)
@@ -47,7 +61,11 @@ Fields = fieldnames(eval(Path));
     end
 Prompt = append(Prompt,'\n');
 Entry = input(Prompt);
-fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+47));
+fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+71));
+if Entry > 0 && Entry < height(Fields)+1
+b = 1;
+end
+end
 Fields = Fields(Entry);
 Donnee.SSType = char(Fields);
     
@@ -56,8 +74,10 @@ end
 
 % Largeur du pont
 if isempty(Donnee.TypDonn)
+b = 0;
+while b == 0
 disp(append('Disposition ',int2str(a),' : ',Path));
-disp('-----------------------------');
+disp("-----------------------------------------------------");
 Prompt = 'Largeur du Pont : ';
 Fields = fieldnames(eval(Path));
     for i = 1:height(Fields)
@@ -65,7 +85,11 @@ Fields = fieldnames(eval(Path));
     end
 Prompt = append(Prompt,'\n');
 Entry = input(Prompt);
-fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+47));
+fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+71));
+if Entry > 0 && Entry < height(Fields)+1
+b = 1;
+end
+end
 Fields = Fields(Entry);
 Donnee.Largeur = char(Fields);
     
@@ -74,8 +98,10 @@ end
 
 % Disposition du trafic
 if isempty(Donnee.TypDonn)
+b = 0;
+while b == 0
 disp(append('Disposition ',int2str(a),' : ',Path));
-disp('-----------------------------');
+disp("-----------------------------------------------------");
 Prompt = 'Disposition du trafic : ';
 Fields = fieldnames(eval(Path));
     for i = 1:height(Fields)
@@ -83,7 +109,11 @@ Fields = fieldnames(eval(Path));
     end
 Prompt = append(Prompt,'\n');
 Entry = input(Prompt);
-fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+47));
+fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+71));
+if Entry > 0 && Entry < height(Fields)+1
+b = 1;
+end
+end
 Fields = Fields(Entry);
 Donnee.DispoTrafic = char(Fields);
     
@@ -92,8 +122,10 @@ end
 
 % Conditions d'appuis
 if isempty(Donnee.TypDonn) || (strcmp(Donnee.BridgeType,'Slab') && strcmp(Donnee.TypDonn,'AGB'))
+b = 0;
+while b == 0
 disp(append('Disposition ',int2str(a),' : ',Path));
-disp('-----------------------------');
+disp("-----------------------------------------------------");
 Prompt = 'Conditions d''appuis : ';
 Fields = fieldnames(eval(Path));
     for i = 1:height(Fields)
@@ -101,7 +133,11 @@ Fields = fieldnames(eval(Path));
     end
 Prompt = append(Prompt,'\n');
 Entry = input(Prompt);
-fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+47));
+fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+71));
+if Entry > 0 && Entry < height(Fields)+1
+b = 1;
+end
+end
 Fields = Fields(Entry);
 Donnee.Support = char(Fields);
     
@@ -110,8 +146,10 @@ end
 
 % Positition du point d'étude
 if isempty(Donnee.TypDonn) || (strcmp(Donnee.BridgeType,'Slab') && strcmp(Donnee.TypDonn,'AGB')) || (strcmp(Donnee.BridgeType,'Multi') && strcmp(Donnee.TypDonn,'AGB'))
+b = 0;
+while b == 0
 disp(append('Disposition ',int2str(a),' : ',Path));
-disp('-----------------------------');
+disp("-----------------------------------------------------");
 Prompt = 'Position d''étude : ';
 Fields = fieldnames(eval(Path));
     for i = 1:height(Fields)
@@ -119,7 +157,11 @@ Fields = fieldnames(eval(Path));
     end
 Prompt = append(Prompt,'\n');
 Entry = input(Prompt);
-fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+47));
+fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+71));
+if Entry > 0 && Entry < height(Fields)+1
+b = 1;
+end
+end
 Fields = Fields(Entry);
 Donnee.TransPos = char(Fields);
     
@@ -128,8 +170,10 @@ end
 
 
 % Effort étudié
+b = 0;
+while b == 0
 disp(append('Disposition ',int2str(a),' : ',Path));
-disp('-----------------------------');
+disp("-----------------------------------------------------");
 Prompt = 'Effort étudié : ';
 Fields = fieldnames(eval(Path));
     for i = 1:height(Fields)
@@ -137,15 +181,21 @@ Fields = fieldnames(eval(Path));
     end
 Prompt = append(Prompt,'\n');
 Entry = input(Prompt);
-fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+47));
+fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+71));
+if Entry > 0 && Entry < height(Fields)+1
+b = 1;
+end
+end
 Fields = Fields(Entry);
 Donnee.AEstress = char(Fields);
     
 Path = append(Path,'.',char(Fields));
 
 % Portée
+b = 0;
+while b == 0
 disp(append('Disposition ',int2str(a),' : ',Path));
-disp('-----------------------------');
+disp("-----------------------------------------------------");
 Prompt = 'Portée : ';
 Fields = fieldnames(eval(Path));
     for i = 1:height(Fields)
@@ -153,7 +203,11 @@ Fields = fieldnames(eval(Path));
     end
 Prompt = append(Prompt,'\n');
 Entry = input(Prompt);
-fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+47));
+fprintf(repmat('\b',1,length(Prompt)+1+length(Path)+71));
+if Entry > 0 && Entry < height(Fields)+1
+b = 1;
+end
+end
 Fields = Fields(Entry);
 Donnee.Span = char(Fields);
     
@@ -169,10 +223,12 @@ Y = Graph(:,2:end);
 figure(1);
 xlabel('Longueur du pont [m]');
 ylabel('Poids d influence');
-TracGraph = {'-';'--';':';'-.';'*';'s';'d'};
+TracGraph = {'-';'--';':';'-.';'-';'-';'-'};
+MarcGraph = {'none';'none';'none';'none';'o';'+';'.'};
+CoulGraph = [0 0.4470 0.7410;0.8500 0.3250 0.0980;0.9290 0.6940 0.1250;0.4940 0.1840 0.5560;0.4660 0.6740 0.1880;0.3010 0.7450 0.9330;0.6350 0.0780 0.1840];
 
 for i=1:width(Y)
-plot(X,Y(:,i),char(TracGraph(a)));
+plot(X,Y(:,i),'Color',CoulGraph(i,:),'LineStyle',char(TracGraph(a)),'Marker',char(MarcGraph(a)));
 hold on;
 Legende{i} = append('Voie',int2str(i));
 end
@@ -193,14 +249,33 @@ a = a+1;
 
 disp(append('Disposition ',int2str(a-1),' : ',Path,' ','''',char(TracGraph(a-1)),''''));
 % Continue ?
-Prompt = append('Voulez vous ajouter un graphique : 1)Oui 2)Non ',', Nombre de graphique(s) actuellement : ',int2str(a-1),'\n');
+disp("-----------------------------------------------------");
+Prompt = append('Voulez vous ajouter un graphique : 1)Oui 2)Non 3)Supprimer ',', Nombre de graphique(s) actuellement : ',int2str(a-1),'\n');
 Entry = input(Prompt);
+fprintf(repmat('\b',1,103));
 
 if Entry == 2
    a = 0;
+elseif Entry == 3
+   fprintf(repmat('\b',1,54)); 
+   fprintf(repmat('\b',1,20+length(char(TracGraph(a-1)))+length(Path)));
+   h = findobj('Type','line');
+   sizeplotrem = height(h)-sizeplot;
+   h = h(1:sizeplotrem);
+   delete(h);
+   a = a-1;
+   disp("-----------------------------------------------------");
+   Prompt = append('Supprimé! Continuer ? 1)Oui 2)Non ','\n');
+   Entry = input(Prompt);
    fprintf(repmat('\b',1,91));
+   if Entry == 2
+   a = 0;
+   disp("-----------------------------------------------------");
+   end
 else
-fprintf(repmat('\b',1,91));
+fprintf(repmat('\b',1,54)); 
 end
+sizeplot = height(findobj('Type','line'));
+
 
 end
