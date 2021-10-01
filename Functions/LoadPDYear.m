e4d48083317f5359ddd8ef5f4c8dfdf81e3ee927
msjaarda <mjsjaarda@gmail.com>
2021-10-01 11:16:12 +0200

    d

---
+++ Functions/LoadPDYear.m
@@ -0,0 +1,9 @@
+function [PDsy] = LoadPDYear(Name,Year)
+%LOADPDYEAR Loads 1 year of PD (function is an effective way of clearing
+%what you don't want)
+
+load(Name);
+PDsy = PDs(year(PDs.DTS) == Year,:);
+            
+end
+
